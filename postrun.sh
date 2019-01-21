8b7faa2fc1d4397c57a676dca7ea0f6d2e527e47
Weinan Li <l.weinan@gmail.com>
2019-01-21 13:12:56 +0800

    needs restart

--- postrun.sh
+++ postrun.sh
@@ -8,7 +8,9 @@ echo '</configuration>' >> /opt/oozie-4.2.0/conf/oozie-site.xml
 oozied.sh start
 oozie admin -oozie http://localhost:11000/oozie -status
 ./wait-for-it.sh hadoop:54321 -- oozie-setup.sh sharelib create -fs hdfs://hadoop:9000
-
+# needs restart
+oozied.sh stop
+oozied.sh start
 oozie admin -shareliblist
 
 tail -f /dev/null
